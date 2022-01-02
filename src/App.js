e57f55f75451b60f96c5b91127d0e48d57754ed5
shahariyer-as <shahariyer.as@gmail.com>
2022-01-02 11:22:31 +0600

    mega menu add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import './App.css';
 import {
@@ -10,31 +10,33 @@ import {
 } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Nav from './Pages/Shared/Nav/Nav';
 function App() {
   return (
     <div className="App">
-      <h1>app.js</h1>
-      <h2> hello project</h2>
-      <h2>oohhh no</h2>
-     
+
       <BrowserRouter>
-      
-      <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-      <Route
-        path="/home"
-        element={<Home />}
-      />
-      <Route
-        path="/about"
-        element={<About />}
-      />
-      
-      </Routes>
-      
+
+        <Routes>
+          <Route
+            path="/"
+            element={<Nav />}
+          />
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+
+        </Routes>
+
       </BrowserRouter>
     </div>
   );
