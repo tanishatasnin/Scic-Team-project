c12b1b7812be2db5212ece26951ee0bf117341de
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-01-03 13:04:55 +0600

    home updated

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import "./App.css";
 import About from "./Pages/About/About";
 import Galary from "./Pages/Galary/Galary";
 import Home from "./Pages/Home/Home";
-import Nav from "./Pages/Shared/Nav/Nav";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Nav />} />
+          <Route path="/" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
