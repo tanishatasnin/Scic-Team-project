437df71e34a8a09ee6654c94d4139cfbf207f641
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-01-03 13:43:36 +0600

    add bug firebase and login fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import AuthProvider from "./contexts/AuthProvider";
 import About from "./Pages/About/About";
 import Galary from "./Pages/Galary/Galary";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login/Login";
+import Register from "./Pages/Login/Register/Register";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/galary" element={<Galary />} />
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} /> */}
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/galary" element={<Galary />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
