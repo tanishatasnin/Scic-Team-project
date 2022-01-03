f5072b13c8eb3c800edeba949dd2cb53a477a624
TanvirFahimBD <tanvirfahim6939@gmail.com>
2022-01-03 12:20:00 +0600

    merge conflict resolved

--- src/App.js
+++ src/App.js
@@ -1,70 +1,20 @@
-
-import './App.css';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  BrowserRouter
-} from "react-router-dom";
-import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
-<<<<<<< HEAD
-import Nav from './Pages/Shared/Nav/Nav';
-=======
-import Galary from './Pages/Galary/Galary';
->>>>>>> origin/about
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Pages/About/About";
+import Galary from "./Pages/Galary/Galary";
+import Home from "./Pages/Home/Home";
+import Nav from "./Pages/Shared/Nav/Nav";
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
-<<<<<<< HEAD
-
         <Routes>
-          <Route
-            path="/"
-            element={<Nav />}
-          />
-          <Route
-            path="/"
-            element={<Home />}
-          />
-          <Route
-            path="/home"
-            element={<Home />}
-          />
-          <Route
-            path="/about"
-            element={<About />}
-          />
-
+          <Route path="/" element={<Nav />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/galary" element={<Galary />} />
         </Routes>
-
-=======
-      
-      <Routes>
-      <Route
-        path="/"
-        element={<Home />}
-      />
-      <Route
-        path="/galary"
-        element={<Galary />}
-      />
-      <Route
-        path="/home"
-        element={<Home />}
-      />
-      <Route
-        path="/about"
-        element={<About />}
-      />
-      
-      </Routes>
-      
->>>>>>> origin/about
       </BrowserRouter>
     </div>
   );
